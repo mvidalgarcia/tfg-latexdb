52789721c12fdf722fc52eda003c5d3217d77416
JL Diaz (linux) <jldiaz@uniovi.es>
2014-05-28 12:56:33 +0200

    Controller now uses $http service
    It GETs an example JSON static file

--- javascript/controllers.js
+++ javascript/controllers.js
@@ -2,21 +2,10 @@
 
 var problemsCollection = angular.module("problemsCollection", []);
 
-problemsCollection.controller('ProblemListCtrl', function($scope) {
-    $scope.problemas = [
-      { 'resumen': 'Problema sobre AJAX',
-        'tags': ['javascript', 'html', 'XHR'],
-        'id': 100 },
-      { 'resumen': 'Problema sobre AngularJS',
-        'tags': ['javascript', 'html', 'XHR'],
-        'id': 101 },
-      { 'resumen': 'Problema sobre HTML5',
-        'tags': ['html'],
-        'id': 102 },
-      { 'resumen': 'Problema sobre Proxies',
-        'tags': ['http'],
-        'id': 103 }
-    ];
+problemsCollection.controller('ProblemListCtrl', function($scope, $http) {
+    $http.get("preguntas-ejemplo.json").success(function(data){
+        console.log(data);
+    });
   });
 
 
