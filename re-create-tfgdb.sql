f5c60eb4d0e0c8c04534c2a523ba4cc3f40770d9
Marco Vidal <marcovidal67@gmail.com>
2014-06-26 03:16:27 +0200

    Added user creation to re-create-tfgdb.sql.

--- re-create-tfgdb.sql
+++ re-create-tfgdb.sql
@@ -1,3 +1,8 @@
+-- CreaciÃn de la base de datos
 drop database if exists tfgdb;
 create database tfgdb CHARACTER SET utf8 COLLATE utf8_general_ci;
 use tfgdb;
+
+-- CreaciÃ³n del usuario dotandole de permisos
+grant usage on *.* to tfguser@localhost identified by 'tfgpass';
+grant all privileges on tfgdb.* to tfguser@localhost;
