4c9785725ef56230fdb46653c7d7dd155bf097a9
Marco Vidal <marcovidal67@gmail.com>
2014-07-01 02:28:50 +0200

    Unnecessary files removed.

---
+++ fonts/README.md
@@ -0,0 +1,1 @@
+En este directorio se encuentran los ficheros necesarios para cargar los iconos Font Awesome Icons utilizados en la GUI.
--- mappers/problema_mapper.php
+++ mappers/problema_mapper.php
@@ -14,61 +14,6 @@ class ProblemaMapper
 		self::$dbh = Database::getConnection();
     }
 
-/* TODO: ESTA PARTE SE ELIMINARÁ, SE USA COMO GUÍA DE ALGUNAS FUNCIONES.	
-
-	
-	public function Insert($Problema)
-    {
-        $STH = self::$dbh->prepare(
-         "INSERT INTO problema (enunciado_general, resumen) value (:enunciado_general, :resumen)"); 
-        $STH->bindParam(':enunciado_general', $Problema->enunciado_general);
-        $STH->bindParam(':resumen', $Problema->resumen);
-        $STH->execute(); 
-        $Problema->id_problema = self::$dbh->lastInsertId();
-    }
-
-	public function Update($Problema)
-    {
-        $STH = self::$dbh->prepare(
-         "UPDATE problema SET enunciado_general = :enunciado_general, resumen = :resumen WHERE id_problema = :id_problema"); 
-        $STH->bindParam(':enunciado_general', $Problema->enunciado_general);
-        $STH->bindParam(':resumen', $Problema->resumen);
-		$STH->execute(); 
-    } 
-
-	public function Save($Problema)
-    {
-        if ( ($Problema->id_problema === NULL) or (empty($Problema->id_problema)) )
-            self::Insert($Problema);
-        else
-            self::Update($Problema);
-	}
-	
-	public function Delete($Problema)
-    {
-        $STH = self::$dbh->prepare('DELETE FROM problema WHERE id_problema = :id_problema');
-        $STH->bindParam(':id_problema', $Problema->id_problema);
-        $STH->execute(); 
-    }
-
-	public function FindById($id)
-    {
-        $STH = self::$dbh->prepare('SELECT * FROM problema WHERE id_problema = :id');
-        $STH->bindParam(':id', $id);
-        $STH->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Problema');  
-        $STH->execute(); 
-        return $STH->fetch();
-    }
-
-    public function FindAll()
-    {
-        $STH = self::$dbh->prepare('SELECT * FROM problema');
-        //$STH->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Problema');  
-        $STH->execute();
-        return $STH->fetchAll();
-    } 
-*/
-
 
 	/* Función que obtiene toda la información nececesaria de un problema 
 	 * incluyendo sus preguntas y sus tags asociados */
